4b930e8893c0e86e563b722ba39484c50c69a4fa
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-04 23:33:45 +0900

    Update 2_AddTwosNumbers(Medium).py

--- Medium/2_AddTwosNumbers(Medium).py
+++ Medium/2_AddTwosNumbers(Medium).py
@@ -21,10 +21,11 @@ class Solution:
             newNode = ListNode(i)
             ptr.next = newNode
             ptr = ptr.next
-        return head.next
+        return head.next #最初のnodeには０を代入してしまったから
 
     
     def convert(self, l):
+        #ここで一旦linkedlistをstr型の値に変えてarryに入れる(その後joinを使うため)
         num = []
         while l:
             tmp = l.val
