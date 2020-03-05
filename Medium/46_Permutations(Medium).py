f549650f07cf33b605791fa82c04572a1eb7d616
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-05 15:20:47 +0900

    Create 46_Permutations(Medium).py

---
+++ Medium/46_Permutations(Medium).py
@@ -0,0 +1,22 @@
+#Runtime: 40 ms, faster than 57.02% of Python3 online submissions for Permutations.
+#Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Permutations.
+
+from typing import List
+from itertools import permutations
+
+class Solution:
+    def permute(self,nums: List[int]) -> List[List[int]]:
+        ans = []
+        for i in permutations(nums):
+            ans.append(list(i))
+        return ans
+
+
+if __name__ =='__main__':
+    nums: List[int] = [None] * int(input('配列の個数：'))
+    for n in range(len(nums)):
+        nums[n] = int(input(f'{n +1}個目の要素'))
+    s = Solution()
+    print(*s.permute(nums), sep ='\n')
+
+#*はアンパック（二次元配列）
