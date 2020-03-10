b40e6ebf3644267b00b81d4f5d7ada8478aa72f8
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-11 03:57:20 +0900

    Create 81_SearchinRotatedSortedArrayII(Medium).py

---
+++ Medium/81_SearchinRotatedSortedArrayII(Medium).py
@@ -0,0 +1,16 @@
+
+#意味が分からない。
+#恐らくLeetcode史上一番簡単な問題。
+#Time Complexity O(logN)以下で探せ、とかだったら（それでも２分探索を使えばいいだけなので超簡単だが）まだわかるが、なんの指示もない。
+#現にLinear Searchでやっても91％のpythonユーザーより早いRun timeになった。
+
+#Runtime: 48 ms, faster than 91.00% of Python3 online submissions for Search in Rotated Sorted Array II.
+#Memory Usage: 13.1 MB, less than 100.00% of Python3 online submissions for Search in Rotated Sorted Array II.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        for i in nums:
+            if i == target:
+                return True
+        else:
+            return False
