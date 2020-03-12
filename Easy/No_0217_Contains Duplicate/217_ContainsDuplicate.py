809fbf8224b850444196ff535cb584ee75c04b83
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-12 11:05:27 +0900

    Update and rename Easy/217_ContainsDuplicate(Easy).py to Easy/No_0217_Contains Duplicate/217_ContainsDuplicate.py

---
+++ Easy/No_0217_Contains Duplicate/217_ContainsDuplicate.py
@@ -0,0 +1,10 @@
+
+
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == len(set(nums)):
+            return False 
+        else:
+            return True
+        
