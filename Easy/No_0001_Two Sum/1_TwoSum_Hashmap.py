14b45edf77bd2634194d02b9818fc48ec82358a7
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-14 05:51:45 +0900

    Create 1_TwoSum_Hashmap.py

---
+++ Easy/No_0001_Two Sum/1_TwoSum_Hashmap.py
@@ -0,0 +1,28 @@
+#リストのset化とほぼ計算量、メモリは同じ（むしろ少し負けてる）だが、こちらの方がスマートな解法である
+
+# Time Complexity : O(N)
+# Space Complexity : O(N)
+
+#Runtime: 52 ms, faster than 54.83% of Python3 online submissions for Two Sum.
+#Memory Usage: 14.2 MB, less than 54.65% of Python3 online submissions for Two Sum.
+
+a = [2,7,11,15]
+from typing import MutableSequence
+
+class Solution:
+    def twoSum(self, nums: MutableSequence, target: int):
+        #hashmap = dict.fromkeys(nums, 0)
+        ans = []
+        hashmap = {}
+        for key, val in enumerate(nums):
+            if hashmap.get(val) is None:
+                hashmap[target - val] = key
+            else:
+                ans = [hashmap[val], key]
+                break
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum(a , 9))
