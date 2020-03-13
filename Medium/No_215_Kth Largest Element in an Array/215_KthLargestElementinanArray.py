1d4a0e4a2fa0403b2c85d397c3923c7f9fcd7084
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-13 13:34:25 +0900

    Update 215_KthLargestElementinanArray.py

--- Medium/No_215_Kth Largest Element in an Array/215_KthLargestElementinanArray.py
+++ Medium/No_215_Kth Largest Element in an Array/215_KthLargestElementinanArray.py
@@ -1,3 +1,8 @@
+#ヒープソート＋再帰を使った方法もあるのであとで復習する
+
+#Runtime: 68 ms, faster than 57.03% of Python3 online submissions for Kth Largest Element in an Array.
+#Memory Usage: 14 MB, less than 27.50% of Python3 online submissions for Kth Largest Element in an Array.
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         hashmap = dict.fromkeys(list(set(nums)), 0)
