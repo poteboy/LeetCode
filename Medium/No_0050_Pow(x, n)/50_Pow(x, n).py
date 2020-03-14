57b96c8f545e7c95400020843fb61905d44917f1
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-14 10:31:50 +0900

    Create 50_Pow(x, n).py

---
+++ Medium/No_0050_Pow(x, n)/50_Pow(x, n).py
@@ -0,0 +1,8 @@
+
+#Runtime: 28 ms, faster than 67.16% of Python3 online submissions for Pow(x, n).
+#Memory Usage: 13 MB, less than 96.55% of Python3 online submissions for Pow(x, n).
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x ** n
+        
