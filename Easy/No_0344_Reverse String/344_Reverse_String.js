c3385ad2a9b70187f94725ff9229359dcb90fbb8
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-16 04:05:08 +0900

    Create 344_Reverse_String.js

---
+++ Easy/No_0344_Reverse String/344_Reverse_String.js
@@ -0,0 +1,13 @@
+//Runtime: 112 ms, faster than 76.71% of JavaScript online submissions for Reverse String.
+//Memory Usage: 46.7 MB, less than 86.67% of JavaScript online submissions for Reverse String.
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    ans = s.reverse()
+    return ans
+};
+
+//初JavaScriptで解いてみた。
