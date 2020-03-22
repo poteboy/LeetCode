e8fce3d3af8461ec7517b305a2a631c2d045a48d
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-03-22 14:24:26 +0900

    Create 50_Pow(x, n).js

---
+++ Medium/No_0050_Pow(x, n)/50_Pow(x, n).js
@@ -0,0 +1,12 @@
+//Runtime: 112 ms, faster than 12.35% of JavaScript online submissions for Pow(x, n).
+//Memory Usage: 33.4 MB, less than 100.00% of JavaScript online submissions for Pow(x, n).
+
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x, n)
+};
