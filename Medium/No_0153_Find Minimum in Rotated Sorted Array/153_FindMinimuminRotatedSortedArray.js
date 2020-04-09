db5c1c19b366d49731517862cf9df2e3e557af6f
Keita Furuse <59927325+KeitaFuruse@users.noreply.github.com>
2020-04-09 22:39:03 +0900

    Create 153_FindMinimuminRotatedSortedArray.js

---
+++ Medium/No_0153_Find Minimum in Rotated Sorted Array/153_FindMinimuminRotatedSortedArray.js
@@ -0,0 +1,12 @@
+//Runtime: 56 ms, faster than 58.48% of JavaScript online submissions for Find Minimum in Rotated Sorted Array.
+//Memory Usage: 33.8 MB, less than 87.50% of JavaScript online submissions for Find Minimum in Rotated Sorted Array.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    return Math.min(...nums)
+    
+};
