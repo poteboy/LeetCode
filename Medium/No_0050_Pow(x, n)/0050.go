9e1c176f32e2a307141082848970da023c7ad5c5
Keita Furuse <59927325+poteboy@users.noreply.github.com>
2020-05-22 01:28:46 +0900

    Create 0050.go

---
+++ Medium/No_0050_Pow(x, n)/0050.go
@@ -0,0 +1,7 @@
+//Runtime: 0 ms, faster than 100.00% of Go online submissions for Pow(x, n).
+//Memory Usage: 2 MB, less than 100.00% of Go online submissions for Pow(x, n).
+
+
+func myPow(x float64, n int) float64 {
+    return math.Pow(x, float64(n))
+}
